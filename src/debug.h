45fa1a9ea062aa7fed8087c5f9c743d3fcfc9e9c
abdousam <abdou.sam7@gmail.com>
2020-04-01 18:25:04 +0100

    Update debug with event_dbg_msg

--- src/debug.h
+++ src/debug.h
@@ -1,6 +1,9 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
+
+#define event_dbg_msg(format, ...) dbg_msg(format, ##__VA_ARGS__)
+
 #define dbg_msg(format, ...)     printf(format, ##__VA_ARGS__)
 
 #endif /* DEBUG_H */
